cc01aec26c056c4653887828ba40f8bd93fe9cd8
Ekaterina Verbitskaia <kajigor@gmail.com>
2025-02-21 14:12:06 +0100

    HW02

---
+++ BookStore.hs
@@ -0,0 +1,101 @@
+module BookStore where
+
+import Data.List (intercalate)
+import Data.List.NonEmpty qualified as NE
+
+data Person = Person
+  { firstName :: String,
+    lastName :: String,
+    yearOfBirth :: Int
+  }
+
+instance Show Person where
+  show p = undefined
+
+-- A book has at least one author, thus we use `NonEmpty` which is a list with at least one element.
+-- See https://hackage.haskell.org/package/base-4.21.0.0/docs/Data-List-NonEmpty.html.
+-- Use functions which work over `NonEmpty` by prefixing them with `NE.`, for example `NE.map`.
+data Book = Book
+  { title :: String,
+    authors :: NE.NonEmpty Person,
+    yearOfPublication :: Int,
+    price :: Double
+  }
+
+instance Show Book where
+  show b = undefined 
+
+-- The `type` keyword introduces a type alias.
+-- `[Book]` and `Catalog` can be used interchangeably.
+-- In error messages, you will only see `[Book]`
+type Catalog = [Book]
+
+-- We use `OVERLAPPING` here to notify GHC that it should use this instance of `Show` even if 
+-- it sees other possibilities, including for a polymorphic list `[a]`. 
+-- See: https://downloads.haskell.org/ghc/latest/docs/users_guide/exts/instances.html#instance-overlap
+instance {-# OVERLAPPING #-} Show Catalog where
+  show bs = undefined 
+
+-- Find all books which have been published before the given year
+oldBooks :: Int -> Catalog -> Catalog
+oldBooks maxYearOfPublication catalog =
+  undefined 
+
+-- At least one of the authors should satisfy the predicate.
+booksByAuthor :: (Person -> Bool) -> Catalog -> Catalog
+booksByAuthor p catalog =
+  undefined 
+
+-- Apply a given discount to the books which satisfy the predicate
+discount :: Double -> (Book -> Bool) -> Catalog -> Catalog
+discount rate p catalog =
+  undefined 
+
+sampleCatalog :: Catalog
+sampleCatalog =
+  [ Book
+      { title = "Learn You a Haskell for Great Good!",
+        authors = Person "Miran" "Lipovača" 1979 NE.:| [],
+        yearOfPublication = 2011,
+        price = 30.0
+      },
+    Book
+      { title = "Real World Haskell",
+        authors =
+          Person "Bryan" "O'Sullivan" 1975
+            NE.:| [ Person "John" "Goerzen" 1960,
+                    Person "Don" "Stewart" 1950
+                  ],
+        yearOfPublication = 2008,
+        price = 45.0
+      },
+    Book
+      { title = "Haskell Programming from First Principles",
+        authors =
+          Person "Christopher" "Allen" 1985
+            NE.:| [Person "Julie" "Moronuki" 1980],
+        yearOfPublication = 2019,
+        price = 50.0
+      },
+    Book
+      { title = "Haskell in Depth",
+        authors = Person "Vitaly" "Bragilevsky" 1988 NE.:| [],
+        yearOfPublication = 2021,
+        price = 55.0
+      },
+    Book
+      { title = "Parallel and Concurrent Programming in Haskell",
+        authors = Person "Simon" "Marlow" 1960 NE.:| [],
+        yearOfPublication = 2013,
+        price = 40.0
+      }
+  ]
+
+main = do
+  print sampleCatalog
+  putStrLn "Old books:"
+  print $ oldBooks 2015 sampleCatalog
+  putStrLn "Books with young authors:"
+  print $ booksByAuthor (\author -> yearOfBirth author > 1980) sampleCatalog
+  putStrLn "40% off!"
+  print $ discount 0.4 (const True) sampleCatalog
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# fp-2025-cyp-spr
-Introduction to Functional Programming course at NUP
+# HW02 
+
+## Deadline 23:59 26.02.2025
+
+Implement a book store catalog in [BookStore.hs](BookStore.hs). 
+
+1. (2 points) Implement `Show` instances for every datatype. You can, for example, display the sample catalog as shown at the bottom of this document. 
+2. (1 point) Implement `oldBooks` to filter books published after a certain date. 
+3. (2 points) Implement `booksByAuthor` to filter books based on their authors. 
+4. (2 points) Implement `discount` to apply a discount to books which satisfy a predicate. 
+5. (1 point) Provide at least 2 other uses of these functions beside those in `main`. 
+
+Try using higher-order functions whenever possible. Consult Hoogle if you don't immediately know which function to use. 
+
+Sample catalog: 
+
+```
+Learn You a Haskell for Great Good!
+        Authors:
+                Miran Lipovača, born 1979
+        Published: 2011
+        Price: 30.0
+Real World Haskell
+        Authors:
+                Bryan O'Sullivan, born 1975
+                John Goerzen, born 1960
+                Don Stewart, born 1950
+        Published: 2008
+        Price: 45.0
+Haskell Programming from First Principles
+        Authors:
+                Christopher Allen, born 1985
+                Julie Moronuki, born 1980
+        Published: 2019
+        Price: 50.0
+Haskell in Depth
+        Authors:
+                Vitaly Bragilevsky, born 1988
+        Published: 2021
+        Price: 55.0
+Parallel and Concurrent Programming in Haskell
+        Authors:
+                Simon Marlow, born 1960
+        Published: 2013
+        Price: 40.0
+```
