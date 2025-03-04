af7e4d8bcb219819e1e2dfc7cc85720aaecb205e
andreystd <ogurcovwm@gmail.com>
2025-03-04 21:27:21 +0200

    HW-02 - Ogurtsov Andrei

--- BookStore.hs
+++ BookStore.hs
@@ -1,6 +1,6 @@
 module BookStore where
 
-import Data.List (intercalate)
+import Data.List ( intercalate, isInfixOf )
 import Data.List.NonEmpty qualified as NE
 
 data Person = Person
@@ -10,7 +10,7 @@ data Person = Person
   }
 
 instance Show Person where
-  show p = undefined
+  show p = firstName p ++ " " ++ lastName p ++ ", born " ++ show (yearOfBirth p)
 
 -- A book has at least one author, thus we use `NonEmpty` which is a list with at least one element.
 -- See https://hackage.haskell.org/package/base-4.21.0.0/docs/Data-List-NonEmpty.html.
@@ -23,33 +23,37 @@ data Book = Book
   }
 
 instance Show Book where
-  show b = undefined 
+  show b =
+    title b
+      ++ "\n\tAuthors:\n"
+      ++ concatMap (\a -> "\t\t" ++ show (firstName a) ++ " " ++ show (lastName a) ++ ", born " ++ show (yearOfBirth a) ++ "\n") (NE.toList $ authors b)
+      ++ "\tPublished: "
+      ++ show (yearOfPublication b)
+      ++ "\n\tPrice: "
+      ++ show (price b)
 
 -- The `type` keyword introduces a type alias.
 -- `[Book]` and `Catalog` can be used interchangeably.
 -- In error messages, you will only see `[Book]`
 type Catalog = [Book]
 
--- We use `OVERLAPPING` here to notify GHC that it should use this instance of `Show` even if 
--- it sees other possibilities, including for a polymorphic list `[a]`. 
+-- We use `OVERLAPPING` here to notify GHC that it should use this instance of `Show` even if
+-- it sees other possibilities, including for a polymorphic list `[a]`.
 -- See: https://downloads.haskell.org/ghc/latest/docs/users_guide/exts/instances.html#instance-overlap
 instance {-# OVERLAPPING #-} Show Catalog where
-  show bs = undefined 
+  show = concatMap (\book -> show book ++ "\n")
 
 -- Find all books which have been published before the given year
 oldBooks :: Int -> Catalog -> Catalog
-oldBooks maxYearOfPublication catalog =
-  undefined 
+oldBooks maxYearOfPublication = filter (\book -> yearOfPublication book < maxYearOfPublication)
 
 -- At least one of the authors should satisfy the predicate.
 booksByAuthor :: (Person -> Bool) -> Catalog -> Catalog
-booksByAuthor p catalog =
-  undefined 
+booksByAuthor p = filter (any p . authors)
 
 -- Apply a given discount to the books which satisfy the predicate
 discount :: Double -> (Book -> Bool) -> Catalog -> Catalog
-discount rate p catalog =
-  undefined 
+discount rate p = map (\book -> if p book then book {price = price book * (1 - rate)} else book)
 
 sampleCatalog :: Catalog
 sampleCatalog =
@@ -91,6 +95,21 @@ sampleCatalog =
       }
   ]
 
+booksByOldAuthors :: Int -> Int -> Catalog -> Catalog
+booksByOldAuthors maxYearOfBirth minYearOfPublication = filter (\book -> any (\author -> yearOfBirth author <= maxYearOfBirth) (authors book) && yearOfPublication book > minYearOfPublication)
+
+
+-- Filter books that contain a specific string in their title
+booksAbout :: String -> Catalog -> Catalog
+booksAbout keyword = filter (\book -> keyword `isInfixOf` title book)
+
+discountOldBooks :: Double -> Int -> Catalog -> Catalog
+discountOldBooks rate year = discount rate (\book -> yearOfPublication book < year)
+
+findBooksByAuthor :: String -> String -> Catalog -> Catalog
+findBooksByAuthor fName lName = booksByAuthor (\author -> firstName author == fName && lastName author == lName)
+
+
 main = do
   print sampleCatalog
   putStrLn "Old books:"
@@ -99,3 +118,11 @@ main = do
   print $ booksByAuthor (\author -> yearOfBirth author > 1980) sampleCatalog
   putStrLn "40% off!"
   print $ discount 0.4 (const True) sampleCatalog
+  putStrLn "Books by old authors and published after the year 2015:"
+  print $ booksByOldAuthors 1980 2015 sampleCatalog
+  putStrLn "Books about Haskell:"
+  print $ booksAbout "Haskell" sampleCatalog
+  putStrLn "30% discount on books published before 2010:"
+  print $ discountOldBooks 0.3 2010 sampleCatalog
+  putStrLn "Books by Miran Lipovača:"
+  print $ findBooksByAuthor "Miran" "Lipovača" sampleCatalog
