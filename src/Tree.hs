49fc7a425b8d6682b46ae518b828909145ddfdad
Artem Abaturov <artemabaturov75@gmail.com>
2025-03-19 01:07:47 +0200

    add solution for the first 4 tasks

--- src/Tree.hs
+++ src/Tree.hs
@@ -16,18 +16,26 @@ node t1 t2 = Node (size t1 + size t2) t1 t2
 
 -- Flattens a tree into a list 
 toList :: Tree a -> [a]
-toList t = undefined 
+toList (Leaf x) = [x]
+toList (Node _ t1 t2) = toList t1 ++ toList t2
 
 -- Fetches the k-th element in the tree 
 fetch :: Int -> Tree a -> a
-fetch k t = undefined 
+fetch k (Leaf x) = x
+fetch k (Node n t1 t2) 
+  | k < size t1 = fetch k t1
+  | otherwise = fetch (k - size t1) t2
 
 -- Updates the k-th element of a tree 
 update :: Show a => Int -> a -> Tree a -> Tree a 
-update k x t = undefined 
+update k x (Leaf _) = Leaf x
+update k x (Node n t1 t2) 
+  | k < size t1 = node (update k x t1) t2
+  | otherwise = node t1 (update (k - size t1) x t2)
 
 -- Checks that the tree is perfect, i.e. it contains exactly 2^n leaves, 
 -- and each node has two well-formed children of equal size. 
 -- Example of a perfect tree: Node 4 (Node 2 (Leaf 'c') (Leaf 'd')) (Node 2 (Leaf 'e') (Leaf 'f'))
 wellFormed :: Tree a -> Bool 
-wellFormed t = undefined 
+wellFormed (Leaf _) = True
+wellFormed (Node n t1 t2) = size t1 == size t2 && wellFormed t1 && wellFormed t2
