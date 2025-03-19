c6ffedd1e585ad09ed0a23a803fcd879770dd48a
Mikhail Maliugin <ghastmisha089@gmail.com>
2025-03-19 05:47:01 +0200

    lists done

--- src/List.hs
+++ src/List.hs
@@ -1,10 +1,16 @@
 module List where
 
+
 -- Generates all permutations of the given list. 
 perms :: [a] -> [[a]]
-perms xs = undefined 
+perms []  = [[]]
+perms xs  = let dropAt :: Int -> [a] -> [a]
+                dropAt ind xs' = take ind xs' ++ drop (ind+1) xs' in
+    [(xs !! x):xs' | x <- [0..length xs-1], xs' <- perms (dropAt x xs)]
 
 -- Concatenate the shortest prefix of xss whose total sum is positive. 
 -- If no sum is positive, then the whole list is concatenated.
 collapse :: [[Int]] -> [Int]
-collapse xss = undefined 
+collapse xss =
+    head ([concat (take n xss) | n <- [1..length xss-1], sum (concat (take n xss)) > 0] ++ [concat xss])
+
