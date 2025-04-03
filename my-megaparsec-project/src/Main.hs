3d175e979d018147a1e9fb1ef256242cd3b4e48f
andreystd <ogurcovwm@gmail.com>
2025-04-03 12:14:58 +0300

    Add tests

--- my-megaparsec-project/src/Main.hs
+++ my-megaparsec-project/src/Main.hs
@@ -2,11 +2,8 @@
 
 module Main (main) where
 
-import Control.Monad (void)
-import Data.Char (isDigit)
 import Data.Text (Text)
 import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -22,6 +19,25 @@ data Expr
   | Div Expr Expr
   deriving (Eq, Show)
 
+evalExpr :: Expr -> Int
+evalExpr (IntLit n) = n
+evalExpr (Sum a b) = evalExpr a + evalExpr b
+evalExpr (Sub a b) = evalExpr a - evalExpr b
+evalExpr (Prod a b) = evalExpr a * evalExpr b
+evalExpr (Div a b) = evalExpr a `div` evalExpr b
+
+prettyPrintExpr :: Expr -> String
+prettyPrintExpr (IntLit n) = show n
+prettyPrintExpr (Sum a b)  = prettyPrintExpr a ++ " + " ++ prettyPrintExpr b
+prettyPrintExpr (Sub a b)  = prettyPrintExpr a ++ " - " ++ prettyPrintParen b
+prettyPrintExpr (Prod a b) = prettyPrintParen a ++ " * " ++ prettyPrintParen b
+prettyPrintExpr (Div a b)  = prettyPrintParen a ++ " / " ++ prettyPrintParen b
+
+prettyPrintParen :: Expr -> String
+prettyPrintParen e = case e of
+  IntLit _ -> prettyPrintExpr e
+  _        -> "(" ++ prettyPrintExpr e ++ ")"
+
 spaceConsumer :: Parser ()
 spaceConsumer = L.space space1 empty empty
 
@@ -36,7 +52,8 @@ parseInt = do
 parseNegative :: Parser Expr
 parseNegative = do
   _ <- symbol '-'
-  Sub (IntLit 0) <$> parseInt
+  IntLit value <- parseInt
+  return (IntLit (-value))
 
 parseParens :: Parser Expr
 parseParens = do
@@ -68,8 +85,29 @@ parseExpr = do
     return (op, expr)
   return $ foldl (\acc (op, x) -> op acc x) first rest
 
+runTest :: String -> Int -> IO ()
+runTest input expected =
+  case runParser parseExpr "" (T.pack input) of
+    Left err -> putStrLn $ "Parse error: " ++ errorBundlePretty err
+    Right expr -> do
+      print expr
+      let computed = evalExpr expr
+      let passed = computed == expected
+      putStrLn $
+        prettyPrintExpr expr ++ " == " ++ show computed ++ " <- " ++ show passed ++ "\n"
+
 main :: IO ()
 main = do
-  print $ runParser parseExpr "" (T.pack "-1 + 2 *3 * 4 +-2")
-  print $ runParser parseExpr "" (T.pack "((-1) + (2 * 3) * 4 + (-2))")
-  print $ runParser parseExpr "" (T.pack "(-1 + 2) * 3 * (4 + (2))")
+  runTest "1 + 2 * 3" 7
+  runTest "-1 + 2 * 3" 5
+  runTest "-(1 + 2) * 3" (-9)
+  runTest "10 / 2 + 1" 6
+  runTest "2 * (3 + 4)" 14
+  runTest "1 + 2 + 3 + 4" 10
+  runTest "18 / 3 / 3" 2
+  runTest "-3 * -2" 6
+  runTest "-3 * (2 + 1)" (-9)
+  runTest "-1 + 2 *3 * 4 +-2" 21
+  runTest "((-1) + (2 * 3) * 4 + (-2))" 21
+  runTest "((-1) + (2 * 3) * 4 + (-2))" 21
+  runTest "(-1 + 2) * 3 * (4 + (2))" 18
